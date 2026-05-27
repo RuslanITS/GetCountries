f5f15a3ec241b9c4692653ba71851ae50cc377ba
Ruslan <rcernov442@gmail.com>
2026-05-28 05:01:48 +0600

    added search for neighbors

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import { useEffect, useState } from "react";
 import { Col, Container, ListGroup, Row, Spinner } from "react-bootstrap";
 
 const BASE_URL = 'https://restcountries.com';
-const ALL_COUNTRIES_URL = '/v3.1/all?fields=name';
+const ALL_COUNTRIES_URL = '/v3.1/all?fields=name,capital,population,region,flags,borders,cca3';
 
 type Country = {
   name: { common: string };
+  capital?: string[];
+  population: number;
+  region: string;
+  borders?: string[];
+  cca3: string;
+  flags: { png: string };
 };
 
 const App = () => {
 
   const [countries, setCountries] = useState<Country[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -41,6 +48,17 @@ const App = () => {
 
   }, []);
 
+  const getBorderCountryName =
+    (borderCode: string): string => {
+
+      const country = countries.find(
+        (country) =>
+          country.cca3 === borderCode
+      );
+
+      return (country?.name.common || borderCode);
+    };
+
   return (
 
     <Container className="py-5">
@@ -51,10 +69,7 @@ const App = () => {
 
         <div className="text-center">
 
-          <Spinner
-            animation="border"
-            variant="primary"
-          />
+          <Spinner animation="border" variant="danger" />
 
         </div>
 
@@ -71,13 +86,24 @@ const App = () => {
 
             <ListGroup variant="flush" className="h-100 overflow-auto">
 
-              {countries.map((country) => (
+              {countries.map((country) => {
 
-                <ListGroup.Item key={country.name.common} action>
-                  {country.name.common}
-                </ListGroup.Item>
+                const {name} = country;
 
-              ))}
+                return (
+
+                  <ListGroup.Item
+                    key={name.common}
+                    onClick={() =>
+                      setSelectedCountry(country)
+                    }
+                    action
+                  >
+                    {name.common}
+                  </ListGroup.Item>
+
+                );
+              })}
 
             </ListGroup>
 
@@ -92,7 +118,58 @@ const App = () => {
             "
           >
 
-            <h3 className="text-muted">Set counties</h3>
+            {selectedCountry ? (
+
+              <div className="text-center">
+
+                <img
+                  src={selectedCountry.flags.png}
+                  alt={selectedCountry.name.common}
+                  className="mb-4 rounded shadow"
+                  style={{
+                    width: "250px",
+                  }}
+                />
+
+                <h2 className="mb-3">{selectedCountry.name.common}</h2>
+
+                <p>
+                  <strong>Capital : </strong>
+                  {selectedCountry.capital?.[0]}
+                </p>
+
+                <p>
+                  <strong>Population : </strong>
+                  {selectedCountry.population}
+                </p>
+
+                <p>
+                  <strong>Region : </strong>
+                  {selectedCountry.region}
+                </p>
+
+                <p><strong>Borders:</strong></p>
+
+                <ul>
+
+                  {selectedCountry.borders?.map((border) => (
+
+                    <li
+                      key={border}>
+                      {getBorderCountryName(border)}
+                    </li>
+
+                  ))}
+
+                </ul>
+
+              </div>
+
+            ) : (
+
+              <h3 className="text-muted">Choose country</h3>
+
+            )}
 
           </Col>
 
